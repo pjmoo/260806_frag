7e087bd1a0c6f55dc7858a03017090f89be57eeb
pjmoo <00pjmoooo@gmail.com>
2026-08-06 15:59:54 +0900

    Initial commit

---
+++ src/main/java/org/example/frag/FragApplication.java
@@ -0,0 +1,13 @@
+package org.example.frag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FragApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FragApplication.class, args);
+    }
+
+}
