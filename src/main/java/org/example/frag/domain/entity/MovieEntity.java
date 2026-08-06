d13212afd63fb04dff7010473efa5fdfc1fcf440
pjmoo <00pjmoooo@gmail.com>
2026-08-06 15:59:55 +0900

    feat: Add Lombok annotations to MovieEntity

--- src/main/java/org/example/frag/domain/entity/MovieEntity.java
+++ src/main/java/org/example/frag/domain/entity/MovieEntity.java
@@ -1,9 +1,17 @@
 package org.example.frag.domain.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "movies")
+@Getter
+@Builder
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+@AllArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public class MovieEntity extends BaseEntity {
     private String title;
     private int price;
